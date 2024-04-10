b96ecd2ca5c01ea42a99e8c8c26dafa9c4f853be
jcardina <jcardina@student.42roma.it>
2024-04-10 12:33:06 +0200

    fatto ex00 cpp03

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jacopo <jacopo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 20:01:31 by jacopo            #+#    #+#             */
-/*   Updated: 2024/04/09 20:43:49 by jacopo           ###   ########.fr       */
+/*   Updated: 2024/04/10 12:07:19 by jacopo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class ClapTrap
 {
 private:
 	std::string	_name;
-	int			_hitPoints = 10;
-	int			_energyPoints = 10;
-	int			_attackDamage = 0;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
 
 public:
 	ClapTrap(std::string name);
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jacopo <jacopo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 20:36:50 by jacopo            #+#    #+#             */
-/*   Updated: 2024/04/09 20:41:36 by jacopo           ###   ########.fr       */
+/*   Updated: 2024/04/10 12:21:51 by jacopo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,16 @@ int main(void)
 	std::cout << std::endl;
 	ClapTrap c1(name1);
 
-	std::cout << "insert a cucciola name: ";
+	std::cout << "insert a cucciola name (she will die): ";
 	std::cin >> name2;
 	std::cout << std::endl;
 	ClapTrap c2(name2);
+
+	for(int i = 0; i <= 11; i++)
+	{
+		c1.attack("shrek");
+		c2.takeDamage(2);
+		c1.beRepaired(1);
+	}
+	return(0);
 }
